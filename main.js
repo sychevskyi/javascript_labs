391f934ab3a287a2a0cc38a57214e27fa41951c0
sychevskyi <sychevskyiv@gmail.com>
2020-10-15 04:33:14 +0200

    starting js2

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-const firstImage = document.querySelector("click");
+const firstImage = document.querySelector('click');
 console.dir(click);
+
+firstImage.addEventListener("click", showLighbox);
+function showLighbox() {
+  console.log(firstImage.img);
+}
